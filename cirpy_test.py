c5e0691753eaa533d5a0f32517380d6b566628d8
Matt Swain <m.swain@me.com>
2015-03-20 13:03:10 +0000

    Fix failing test

--- cirpy_test.py
+++ cirpy_test.py
@@ -15,7 +15,7 @@ from urllib2 import HTTPError
 class TestCIRpy(unittest.TestCase):
 
     def setUp(self):
-        self.alanine_smiles = 'CC(C(O)=O)N'
+        self.alanine_smiles = 'N[C@@H](C)C(=O)O'
         self.morphine_inchi = [{'value': 'InChI=1/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13?,16-,17-/m0/s1', 'notation': 'Morphine', 'resolver': 'name_by_cir'}]
         self.tnt_opsin_smiles = '[N+](=O)([O-])C1=C(C(=CC(=C1)[N+](=O)[O-])[N+](=O)[O-])C'
         self.tnt_cir_smiles = 'C1=C(C=C(C(=C1[N+]([O-])=O)C)[N+]([O-])=O)[N+]([O-])=O'
