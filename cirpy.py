b2a399c097afbf637453b940169038fec8f2e107
Matt Swain <m.swain@me.com>
2015-03-20 13:13:40 +0000

    Tidy formatting

--- cirpy.py
+++ cirpy.py
@@ -30,7 +30,7 @@ API_BASE = 'http://cactus.nci.nih.gov/chemical/structure'
 
 
 def resolve(input, representation, resolvers=None, **kwargs):
-    """ Resolve input to the specified output representation """
+    """Resolve input to the specified output representation."""
     resultdict = query(input, representation, resolvers, **kwargs)
     result = resultdict[0]['value'] if resultdict else None
     if result and len(result) == 1:
@@ -39,20 +39,18 @@ def resolve(input, representation, resolvers=None, **kwargs):
 
 
 def query(input, representation, resolvers=None, **kwargs):
-    """ Get all results for resolving input to the specified output representation """
-    apiurl = API_BASE+'/%s/%s/xml' % (urllib2.quote(input), representation)
+    """Get all results for resolving input to the specified output representation."""
+    apiurl = '%s/%s/%s/xml' % (API_BASE, urllib2.quote(input), representation)
     if resolvers:
-        kwargs['resolver'] = ",".join(resolvers)
+        kwargs['resolver'] = ','.join(resolvers)
     if kwargs:
-        apiurl+= '?%s' % urllib.urlencode(kwargs)
+        apiurl += '?%s' % urllib.urlencode(kwargs)
     result = []
     try:
         tree = etree.parse(urllib2.urlopen(apiurl))
-        for data in tree.findall(".//data"):
-            datadict = {'resolver':data.attrib['resolver'],
-                        'notation':data.attrib['notation'],
-                        'value':[]}
-            for item in data.findall("item"):
+        for data in tree.findall('.//data'):
+            datadict = {'resolver': data.attrib['resolver'], 'notation': data.attrib['notation'], 'value': []}
+            for item in data.findall('item'):
                 datadict['value'].append(item.text)
             if len(datadict['value']) == 1:
                 datadict['value'] = datadict['value'][0]
@@ -62,18 +60,19 @@ def query(input, representation, resolvers=None, **kwargs):
         pass
     return result if result else None
 
+
 def download(input, filename, format='sdf', overwrite=False, resolvers=None, **kwargs):
-    """ Resolve and download structure as a file """
+    """Resolve and download structure as a file."""
     kwargs['format'] = format
     if resolvers:
-        kwargs['resolver'] = ",".join(resolvers)
-    url = API_BASE+'/%s/file?%s' % (urllib2.quote(input), urllib.urlencode(kwargs))
+        kwargs['resolver'] = ','.join(resolvers)
+    url = '%s/%s/file?%s' % (API_BASE, urllib2.quote(input), urllib.urlencode(kwargs))
     print url
     try:
         servefile = urllib2.urlopen(url)
         if not overwrite and os.path.isfile(filename):
             raise IOError("%s already exists. Use 'overwrite=True' to overwrite it." % filename)
-        file = open(filename, "w")
+        file = open(filename, 'w')
         file.write(servefile.read())
         file.close()
     except urllib2.HTTPError:
@@ -82,7 +81,7 @@ def download(input, filename, format='sdf', overwrite=False, resolvers=None, **k
 
 
 class CacheProperty(object):
-    """ Descriptor for caching Molecule properties. """
+    """Descriptor for caching Molecule properties."""
 
     def __init__(self, func):
         self._func = func
@@ -90,110 +89,133 @@ class CacheProperty(object):
         self.__doc__ = func.__doc__
 
     def __get__(self, obj, obj_class=None):
-        if obj is None: return None
+        if obj is None:
+            return None
         result = obj.__dict__[self.__name__] = self._func(obj)
         return result
 
 
 class Molecule(object):
-    """Class to hold and cache the structure information for a given CIR input"""
+    """Class to hold and cache the structure information for a given CIR input."""
 
     def __init__(self, input, resolvers=None, **kwargs):
-        """ Initialize with a query input """
+        """Initialize with a query input."""
         self.input = input
         self.resolvers = resolvers
         self.kwargs = kwargs
 
     def __repr__(self):
-        return "Molecule(%r, %r)" % (self.input, self.resolvers)
+        return 'Molecule(%r, %r)' % (self.input, self.resolvers)
 
     @CacheProperty
-    def stdinchi(self): return resolve(self.input, 'stdinchi', self.resolvers, **self.kwargs)
+    def stdinchi(self):
+        return resolve(self.input, 'stdinchi', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def stdinchikey(self): return resolve(self.input, 'stdinchikey', self.resolvers, **self.kwargs)
+    def stdinchikey(self):
+        return resolve(self.input, 'stdinchikey', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def smiles(self): return resolve(self.input, 'smiles', self.resolvers, **self.kwargs)
+    def smiles(self):
+        return resolve(self.input, 'smiles', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def ficts(self): return resolve(self.input, 'ficts', self.resolvers, **self.kwargs)
+    def ficts(self):
+        return resolve(self.input, 'ficts', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def ficus(self): return resolve(self.input, 'ficus', self.resolvers, **self.kwargs)
+    def ficus(self):
+        return resolve(self.input, 'ficus', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def uuuuu(self): return resolve(self.input, 'uuuuu', self.resolvers, **self.kwargs)
+    def uuuuu(self):
+        return resolve(self.input, 'uuuuu', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def hashisy(self): return resolve(self.input, 'hashisy', self.resolvers, **self.kwargs)
+    def hashisy(self):
+        return resolve(self.input, 'hashisy', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def sdf(self): return resolve(self.input, 'sdf', self.resolvers, **self.kwargs)
+    def sdf(self):
+        return resolve(self.input, 'sdf', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def names(self): return resolve(self.input, 'names', self.resolvers, **self.kwargs)
+    def names(self):
+        return resolve(self.input, 'names', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def iupac_name(self): return resolve(self.input, 'iupac_name', self.resolvers, **self.kwargs)
+    def iupac_name(self):
+        return resolve(self.input, 'iupac_name', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def cas(self): return resolve(self.input, 'cas', self.resolvers, **self.kwargs)
+    def cas(self):
+        return resolve(self.input, 'cas', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def chemspider_id(self): return resolve(self.input, 'chemspider_id', self.resolvers, **self.kwargs)
+    def chemspider_id(self):
+        return resolve(self.input, 'chemspider_id', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def mw(self): return resolve(self.input, 'mw', self.resolvers, **self.kwargs)
+    def mw(self):
+        return resolve(self.input, 'mw', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def formula(self): return resolve(self.input, 'formula', self.resolvers, **self.kwargs)
+    def formula(self):
+        return resolve(self.input, 'formula', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def h_bond_donor_count(self): return resolve(self.input, 'h_bond_donor_count', self.resolvers, **self.kwargs)
+    def h_bond_donor_count(self):
+        return resolve(self.input, 'h_bond_donor_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def h_bond_acceptor_count(self): return resolve(self.input, 'h_bond_acceptor_count', self.resolvers, **self.kwargs)
+    def h_bond_acceptor_count(self):
+        return resolve(self.input, 'h_bond_acceptor_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def h_bond_center_count(self): return resolve(self.input, 'h_bond_center_count', self.resolvers, **self.kwargs)
+    def h_bond_center_count(self):
+        return resolve(self.input, 'h_bond_center_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def rule_of_5_violation_count(self): return resolve(self.input, 'rule_of_5_violation_count', self.resolvers, **self.kwargs)
+    def rule_of_5_violation_count(self):
+        return resolve(self.input, 'rule_of_5_violation_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def rotor_count(self): return resolve(self.input, 'rotor_count', self.resolvers, **self.kwargs)
+    def rotor_count(self):
+        return resolve(self.input, 'rotor_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def effective_rotor_count(self): return resolve(self.input, 'effective_rotor_count', self.resolvers, **self.kwargs)
+    def effective_rotor_count(self):
+        return resolve(self.input, 'effective_rotor_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def ring_count(self): return resolve(self.input, 'ring_count', self.resolvers, **self.kwargs)
+    def ring_count(self):
+        return resolve(self.input, 'ring_count', self.resolvers, **self.kwargs)
 
     @CacheProperty
-    def ringsys_count(self): return resolve(self.input, 'ringsys_count', self.resolvers, **self.kwargs)
+    def ringsys_count(self):
+        return resolve(self.input, 'ringsys_count', self.resolvers, **self.kwargs)
 
     @property
     def image_url(self):
-        url = API_BASE+'/%s/image' % urllib2.quote(self.input)
+        url = '%s/%s/image' % (API_BASE, urllib2.quote(self.input))
         qsdict = self.kwargs
         if self.resolvers:
-            qsdict['resolver'] = ",".join(self.resolvers)
+            qsdict['resolver'] = ','.join(self.resolvers)
         if qsdict:
             url += '?%s' % urllib.urlencode(qsdict)
         return url
 
     @property
     def twirl_url(self):
-        url = API_BASE+'/%s/twirl' % urllib2.quote(self.input)
+        url = '%s/%s/twirl' % (API_BASE, urllib2.quote(self.input))
         qsdict = self.kwargs
         if self.resolvers:
-            qsdict['resolver'] = ",".join(self.resolvers)
+            qsdict['resolver'] = ','.join(self.resolvers)
         if qsdict:
             url += '?%s' % urllib.urlencode(qsdict)
         return url
 
     def download(self, filename, format='sdf', overwrite=False, resolvers=None, **kwargs):
-        """ Download the resolved structure as a file """
+        """Download the resolved structure as a file."""
         download(self.input, filename, format, overwrite, resolvers, **kwargs)
 
--- cirpy_test.py
+++ cirpy_test.py
@@ -9,14 +9,18 @@ https://github.com/mcs07/CIRpy
 
 import unittest
 
-from cirpy import *
-from urllib2 import HTTPError
+from cirpy import resolve, query, Molecule
+
 
 class TestCIRpy(unittest.TestCase):
 
     def setUp(self):
         self.alanine_smiles = 'N[C@@H](C)C(=O)O'
-        self.morphine_inchi = [{'value': 'InChI=1/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13?,16-,17-/m0/s1', 'notation': 'Morphine', 'resolver': 'name_by_cir'}]
+        self.morphine_inchi = [{
+            'value': 'InChI=1/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13?,16-,17-/m0/s1',
+            'notation': 'Morphine',
+            'resolver': 'name_by_cir'
+        }]
         self.tnt_opsin_smiles = '[N+](=O)([O-])C1=C(C(=CC(=C1)[N+](=O)[O-])[N+](=O)[O-])C'
         self.tnt_cir_smiles = 'C1=C(C=C(C(=C1[N+]([O-])=O)C)[N+]([O-])=O)[N+]([O-])=O'
         self.hash_smiles = 'C#N'
@@ -28,20 +32,20 @@ class TestCIRpy(unittest.TestCase):
         }, {
             'value': 'C1=C(C=C(C(=C1[N+]([O-])=O)C)[N+]([O-])=O)[N+]([O-])=O',
             'notation': '2,4,6-Trinitrotoluene',
-            'resolver':'name_by_cir'
+            'resolver': 'name_by_cir'
         }]
 
     def test_resolve(self):
-        self.assertEqual(resolve('Alanine','smiles'), self.alanine_smiles)
-        self.assertEqual(resolve('aruighaelirugaerg','inchi'), None)
+        self.assertEqual(resolve('Alanine', 'smiles'), self.alanine_smiles)
+        self.assertEqual(resolve('aruighaelirugaerg', 'inchi'), None)
         self.assertEqual(resolve('Alanine', 'arguergbaiurg'), None)
         self.assertEqual(resolve('2,4,6-trinitrotoluene', 'smiles'), self.tnt_opsin_smiles)
-        self.assertEqual(resolve('2,4,6-trinitrotoluene', 'smiles', ['name_by_opsin','name_by_cir']), self.tnt_opsin_smiles)
-        self.assertEqual(resolve('2,4,6-trinitrotoluene', 'smiles', ['name_by_cir','name_by_opsin']), self.tnt_cir_smiles)
+        self.assertEqual(resolve('2,4,6-trinitrotoluene', 'smiles', ['name_by_opsin', 'name_by_cir']), self.tnt_opsin_smiles)
+        self.assertEqual(resolve('2,4,6-trinitrotoluene', 'smiles', ['name_by_cir', 'name_by_opsin']), self.tnt_cir_smiles)
 
     def test_query(self):
-        self.assertEqual(query('Morphine','inchi'), self.morphine_inchi)
-        self.assertEqual(query('sjkvhaldfu','inchi'), None)
+        self.assertEqual(query('Morphine', 'inchi'), self.morphine_inchi)
+        self.assertEqual(query('sjkvhaldfu', 'inchi'), None)
         self.assertEqual(query('Morphine', 'ogiuewrgpw'), None)
         self.assertEqual(query('2,4,6-trinitrotoluene', 'smiles'), self.tnt_query)
 
